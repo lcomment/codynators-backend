da51d2102140d5b9d76505d4bf0c04ba433fa6d7
khsexk <kohyunsuk98@gmail.com>
2022-04-15 20:33:02 +0900

    자동화 배포 구성

--- app/deploy.sh
+++ app/deploy.sh
@@ -4,5 +4,9 @@ REPOSITORY=/home/ec2-user/app/step2/flask/app
 
 cd $REPOSITORY
 
+docker stop codynators-backend
+docker rm codynators-backend
+docker rmi codynators-backend
+
 docker build -t codynators-backend .
 docker run -d -p 443:5000 --name codynators-backend codynators-backend 
