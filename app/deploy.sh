76a611cd62b4eec62f01e8974b06ec2ab77d3c4a
khsexk <kohyunsuk98@gmail.com>
2022-04-15 18:50:35 +0900

    Update deploy.sh

--- app/deploy.sh
+++ app/deploy.sh
@@ -4,8 +4,5 @@ REPOSITORY=/home/ec2-user/app/step2/flask/app
 
 cd $REPOSITORY
 
-python3 -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
 docker build -t codynators-backend .
 docker run  -p 443:5000 --name codynators-backend codynators-backend
