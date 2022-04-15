538672c37ffa1f4877841279122fc75e72473b1a
khsexk <kohyunsuk98@gmail.com>
2022-04-15 18:58:37 +0900

    Update deploy.sh

--- app/deploy.sh
+++ app/deploy.sh
@@ -5,4 +5,4 @@ REPOSITORY=/home/ec2-user/app/step2/flask/app
 cd $REPOSITORY
 
 docker build -t codynators-backend .
-docker run  -p 443:5000 --name codynators-backend codynators-backend
+docker run -d -p 443:5000 --name codynators-backend codynators-backend 
