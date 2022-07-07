1d6ea42f2105e2b7d50a1b2610c4c5f2e56a5a3d
Hyunseok Ko <56003992+khsexk@users.noreply.github.com>
2022-07-07 21:23:13 +0900

    Update deploy.sh

--- app/deploy.sh
+++ app/deploy.sh
@@ -9,4 +9,4 @@ docker rm codynators-backend
 docker rmi codynators-backend
 
 docker build -t codynators-backend .
-docker run -d -p 443:5000 --name codynators-backend codynators-backend 
+docker run -d -p 5000:5000 --name codynators-backend codynators-backend 
